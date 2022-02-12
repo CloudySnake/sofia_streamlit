dce60240ad96eef6fa4be27ecbe77c69a7b6bc72
Martin Campbell <martin@martincampbell.eu>
2022-02-12 12:43:03 +0000

    First setup

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def main():
+    # st.sidebar.title("Pages")
+    # selection = st.sidebar.radio("Select a page", list(PAGES.keys()))
+
+    # clear_cache = st.sidebar.button("Clear Cache")
+    # if clear_cache:
+    #     st.caching.clear_cache()
+
+    # page = PAGES[selection]
+
+    # page.render_page()
+    st.write("Sofia says hello!")
+
+
+if __name__ == "__main__":
+    main()
